f538d87b58aabd3f94fdabbab884dd0b418c0771
Márton Spiteller <spitellermarton@gmail.com>
2025-12-07 15:27:53 +0100

    set up initial styles and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
-import "./App.css";
 import "./components/Sidebar";
+import Sidebar from "./components/Sidebar";
+import styles from "./styles/Main.module.css";
 
 function App() {
 	return (
-		<>
-			<h1>money manager</h1>
-		</>
+		<section>
+			<Sidebar />
+			<main id={styles.container}>
+				<h1>Main Section</h1>
+				<p>
+					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+					quisquam totam nostrum aut dicta eaque magnam quae accusantium quam
+					voluptates dolores, quasi voluptatem cupiditate maiores quidem vel
+					obcaecati minus! Nostrum?
+				</p>
+			</main>
+		</section>
 	);
 }
 
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,5 +1,17 @@
+import styles from "../styles/Sidebar.module.css";
+
 const Sidebar = () => {
-	return <div>Sidebar</div>;
+	return (
+		<div id={styles.container}>
+			<h2>money manager</h2>
+			<ul>
+				<li>Dashboard</li>
+				<li>New</li>
+				<li>Reports</li>
+				<li>Settings</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Sidebar;
