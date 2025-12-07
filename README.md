5bb202c9a13aa67afcf673e1153ed45786207885
Márton Spiteller <spitellermarton@gmail.com>
2025-12-07 14:25:32 +0100

    config eslint, add prettier

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# money-manager
+# money-manager-progressive-web-app
+
+'money manager' is a personal income and expense tracker with offline functionalities and secure, persistent storage of sensitive data, achieved by using service workers, Web Crypto API and IndexedDB.
+
+## stack
+
+| Frontend  | Backend               | Database  |
+|-----------|-----------------------|-----------|
+| React     | no dedicated backend  | IndexedDB |
--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import prettier from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,10 +16,24 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      prettier
     ],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    plugins: {
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      prettier: prettierPlugin
+    },
+    rules: {
+      ...reactHooks.configs.recommended.rules,
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true }
+      ],
+      "prettier/prettier": "warn"
+    }
   },
 ])
