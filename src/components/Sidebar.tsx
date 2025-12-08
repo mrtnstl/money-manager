f69764b9a1e7a425544c2d556bfd6a8ca26347b4
Márton Spiteller <spitellermarton@gmail.com>
2025-12-08 19:47:42 +0100

    add: localization scaffolding

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,28 +1,38 @@
+import { useEffect, useState } from "react";
 import { NavLink, useNavigate } from "react-router-dom";
+import getLocalization from "../utils/getLocalization";
 import styles from "../styles/Sidebar.module.css";
 
 const Sidebar = () => {
 	const navigate = useNavigate();
+	const [localization, setLocalization] = useState({});
+
+	useEffect(() => {
+		getLocalization("en", "sidebar")
+			.then((response) => setLocalization(response))
+			.catch((err) => console.log(err));
+	}, []);
+
 	return (
 		<div id={styles.container}>
 			<h2 id={styles.title} onClick={() => navigate("/", { replace: true })}>
-				money manager
+				{localization.title || "mm"}
 			</h2>
 			<ul>
 				<NavLink to={"/"} className={styles.navlink}>
-					<li>Home</li>
+					<li>{localization.home || "home"}</li>
 				</NavLink>
 				<NavLink to={"/dashboard"} className={styles.navlink}>
-					<li>Dashboard</li>
+					<li>{localization.dashboard || "dashboard"}</li>
 				</NavLink>
 				<NavLink to={"/new"} className={styles.navlink}>
-					<li>New</li>
+					<li>{localization.new || "new"}</li>
 				</NavLink>
 				<NavLink to={"/reports"} className={styles.navlink}>
-					<li>Reports</li>
+					<li>{localization.reports || "reports"}</li>
 				</NavLink>
 				<NavLink to={"/settings"} className={styles.navlink}>
-					<li>Settings</li>
+					<li>{localization.settings || "settings"}</li>
 				</NavLink>
 			</ul>
 		</div>
